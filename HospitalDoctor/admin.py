c73e41253b289676be0dd0d168680aa52be0d1f6
Mahadiur <almahadiurrahman@gmail.com>
2026-01-03 07:35:23 -0800

    Create HospitalDoctor app and defile all condition

--- HospitalDoctor/admin.py
+++ HospitalDoctor/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import hospital_doctor
 
 # Register your models here.
+admin.site.register(hospital_doctor)
--- HospitalDoctor/models.py
+++ HospitalDoctor/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class hospital_doctor(models.Model):
+    Doctor_name = models.CharField(max_length=50)
+    Doctor_designation = models.CharField(max_length=50)
+    Doctor_email = models.CharField(max_length=50, unique=True)
+    Doctor_phone_number = models.CharField(max_length=11)
+    Doctor_salary = models.DecimalField(max_digits=10, decimal_places=3)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'name:' + self.Doctor_name + " " + 'Designation:' + self.Doctor_designation
